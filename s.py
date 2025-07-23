20071a38aff0d9cefa432df568a184282601fea2
Malyala Sai Deepika <deepikamalyala70@gmail.com>
2025-07-23 15:10:35 +0530

    first commit

---
+++ s.py
@@ -0,0 +1,32 @@
+import mlflow.sklearn
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.preprocessing import OneHotEncoder #increasing dimentionality ,convert categorical variables into a binary (0 or 1) representation
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+import mlflow.sklearn
+import joblib
+
+mlflow.sklearn.autolog()
+ 
+df=pd.read_csv("airbnb_listings.csv")
+x=df.drop(columns=["ListingID","PricePerNight"])
+y=df["PricePerNight"]
+
+Preprocessor=ColumnTransformer(
+    [("cat",OneHotEncoder(handle_unknown='ignore'),["City","RoomType"])],
+    remainder="passthrough"
+)
+
+model=Pipeline([
+    ("preprocessor",Preprocessor),
+    ("regression",RandomForestRegressor(n_estimators=100,random_state=42))]
+)
+
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.1)
+
+with mlflow.start_run():  #Runs ML projects on different platforms or environments using mlflow run.
+    model.fit(x_train,y_train)
+    joblib.dump(model,"airbnb.pkl")
+    print("model saved succesfully")
